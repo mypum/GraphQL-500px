4fd36118f75581c88dcf47d50f0a2f447b18bda9
Thongchai Kitiyanantawong <pum@Pums-MacBook-Pro.local>
2017-09-28 02:55:05 +0700

    Make simple graphiql

---
+++ src/data/photos.js
@@ -0,0 +1,190 @@
+export default [
+  {
+    'id': 229781957,
+    'user_id': 23053409,
+    'name': 'Untitled',
+    'description': '',
+    'camera': null,
+    'lens': null,
+    'focal_length': null,
+    'iso': null,
+    'shutter_speed': null,
+    'aperture': null,
+    'times_viewed': 0,
+    'rating': 0,
+    'status': 1,
+    'created_at': '2017-09-27T15:13:54-04:00',
+    'category': 0,
+    'location': null,
+    'latitude': null,
+    'longitude': null,
+    'taken_at': null,
+    'hi_res_uploaded': 0,
+    'for_sale': false,
+    'width': 1738,
+    'height': 1873,
+    'votes_count': 0,
+    'favorites_count': 0,
+    'comments_count': 0,
+    'nsfw': false,
+    'sales_count': 0,
+    'for_sale_date': null,
+    'highest_rating': 0,
+    'highest_rating_date': null,
+    'license_type': 0,
+    'converted': 0,
+    'collections_count': 0,
+    'crop_version': 0,
+    'privacy': false,
+    'profile': true,
+    'for_critique': false,
+    'critiques_callout_dismissed': false,
+    'image_url': 'https://drscdn.500px.org/photo/229781957/w%3D280_h%3D280/v2?client_application_id=42625&webp=true&v=0&sig=01cf5e57ff4704ec2a56ab2d3b1711075fe38f13e27ae6ce3df5f92f47f066ed',
+    'images': [
+      {
+        'size': 3,
+        'url': 'https://drscdn.500px.org/photo/229781957/w%3D280_h%3D280/v2?client_application_id=42625&webp=true&v=0&sig=01cf5e57ff4704ec2a56ab2d3b1711075fe38f13e27ae6ce3df5f92f47f066ed',
+        'https_url': 'https://drscdn.500px.org/photo/229781957/w%3D280_h%3D280/v2?client_application_id=42625&webp=true&v=0&sig=01cf5e57ff4704ec2a56ab2d3b1711075fe38f13e27ae6ce3df5f92f47f066ed',
+        'format': 'jpeg'
+      }
+    ],
+    'url': '/photo/229781957/untitled-by-alhsncyln',
+    'positive_votes_count': 0,
+    'converted_bits': 0,
+    'store_download': false,
+    'store_print': false,
+    'store_license': false,
+    'request_to_buy_enabled': true,
+    'license_requests_enabled': false,
+    'watermark': false,
+    'image_format': 'jpeg',
+    'user': {
+      'id': 23053409,
+      'username': 'alhsncyln1',
+      'firstname': 'ALHSNCYLN',
+      'lastname': '',
+      'city': '',
+      'country': '',
+      'usertype': 0,
+      'fullname': 'ALHSNCYLN',
+      'userpic_url': 'https://pacdn.500px.org/23053409/fe780880e7bfb31327aac16f7d8ad33449f53886/1.jpg?2',
+      'userpic_https_url': 'https://pacdn.500px.org/23053409/fe780880e7bfb31327aac16f7d8ad33449f53886/1.jpg?2',
+      'cover_url': 'https://pacdn.500px.org/23053409/fe780880e7bfb31327aac16f7d8ad33449f53886/cover_2048.jpg?2',
+      'upgrade_status': 0,
+      'store_on': false,
+      'affection': 3420,
+      'avatars': {
+        'default': {
+          'https': 'https://pacdn.500px.org/23053409/fe780880e7bfb31327aac16f7d8ad33449f53886/1.jpg?2'
+        },
+        'large': {
+          'https': 'https://pacdn.500px.org/23053409/fe780880e7bfb31327aac16f7d8ad33449f53886/2.jpg?2'
+        },
+        'small': {
+          'https': 'https://pacdn.500px.org/23053409/fe780880e7bfb31327aac16f7d8ad33449f53886/3.jpg?2'
+        },
+        'tiny': {
+          'https': 'https://pacdn.500px.org/23053409/fe780880e7bfb31327aac16f7d8ad33449f53886/4.jpg?2'
+        }
+      }
+    },
+    'licensing_requested': false,
+    'licensing_suggested': false,
+    'is_free_photo': false
+  },
+  {
+    'id': 229781943,
+    'user_id': 10776781,
+    'name': 'Alhambra',
+    'description': 'Streets of Alabicín',
+    'camera': 'Canon EOS 5D Mark III',
+    'lens': 'TAMRON SP 70-200mm F/2.8 Di VC USD A009',
+    'focal_length': '85',
+    'iso': '200',
+    'shutter_speed': '1/80',
+    'aperture': '18',
+    'times_viewed': 2,
+    'rating': 34.5,
+    'status': 1,
+    'created_at': '2017-09-27T15:13:43-04:00',
+    'category': 8,
+    'location': null,
+    'latitude': 37.181073,
+    'longitude': -3.59265,
+    'taken_at': '2017-08-30T13:07:24-04:00',
+    'hi_res_uploaded': 0,
+    'for_sale': false,
+    'width': 5760,
+    'height': 3840,
+    'votes_count': 1,
+    'favorites_count': 0,
+    'comments_count': 0,
+    'nsfw': false,
+    'sales_count': 0,
+    'for_sale_date': null,
+    'highest_rating': 34.5,
+    'highest_rating_date': '2017-09-27T15:14:11-04:00',
+    'license_type': 0,
+    'converted': 0,
+    'collections_count': 0,
+    'crop_version': 0,
+    'privacy': false,
+    'profile': true,
+    'for_critique': false,
+    'critiques_callout_dismissed': false,
+    'image_url': 'https://drscdn.500px.org/photo/229781943/w%3D280_h%3D280/v2?client_application_id=42625&webp=true&v=0&sig=4913a3f3efb1f1b288b1dbcde0e73aea9b33e06d3d870d94f807fdcfb97067f6',
+    'images': [
+      {
+        'size': 3,
+        'url': 'https://drscdn.500px.org/photo/229781943/w%3D280_h%3D280/v2?client_application_id=42625&webp=true&v=0&sig=4913a3f3efb1f1b288b1dbcde0e73aea9b33e06d3d870d94f807fdcfb97067f6',
+        'https_url': 'https://drscdn.500px.org/photo/229781943/w%3D280_h%3D280/v2?client_application_id=42625&webp=true&v=0&sig=4913a3f3efb1f1b288b1dbcde0e73aea9b33e06d3d870d94f807fdcfb97067f6',
+        'format': 'jpeg'
+      }
+    ],
+    'url': '/photo/229781943/alhambra-by-c%C3%A9dric-fumi%C3%A8re',
+    'positive_votes_count': 1,
+    'converted_bits': 0,
+    'store_download': false,
+    'store_print': false,
+    'store_license': false,
+    'request_to_buy_enabled': true,
+    'license_requests_enabled': true,
+    'store_width': 5760,
+    'store_height': 3840,
+    'watermark': false,
+    'image_format': 'jpeg',
+    'user': {
+      'id': 10776781,
+      'username': 'cedricfumiere',
+      'firstname': 'Cédric',
+      'lastname': 'Fumière',
+      'city': 'Bruxelles',
+      'country': 'Belgique',
+      'usertype': 0,
+      'fullname': 'Cédric Fumière',
+      'userpic_url': 'https://pacdn.500px.org/10776781/b3cf91617228eb199afa7b6312cfdb22a5bf1ac2/1.jpg?1',
+      'userpic_https_url': 'https://pacdn.500px.org/10776781/b3cf91617228eb199afa7b6312cfdb22a5bf1ac2/1.jpg?1',
+      'cover_url': 'https://pacdn.500px.org/10776781/b3cf91617228eb199afa7b6312cfdb22a5bf1ac2/cover_2048.jpg?1',
+      'upgrade_status': 2,
+      'store_on': true,
+      'affection': 17974,
+      'avatars': {
+        'default': {
+          'https': 'https://pacdn.500px.org/10776781/b3cf91617228eb199afa7b6312cfdb22a5bf1ac2/1.jpg?1'
+        },
+        'large': {
+          'https': 'https://pacdn.500px.org/10776781/b3cf91617228eb199afa7b6312cfdb22a5bf1ac2/2.jpg?1'
+        },
+        'small': {
+          'https': 'https://pacdn.500px.org/10776781/b3cf91617228eb199afa7b6312cfdb22a5bf1ac2/3.jpg?1'
+        },
+        'tiny': {
+          'https': 'https://pacdn.500px.org/10776781/b3cf91617228eb199afa7b6312cfdb22a5bf1ac2/4.jpg?1'
+        }
+      }
+    },
+    'licensing_requested': false,
+    'licensing_suggested': false,
+    'is_free_photo': false
+  }
+]
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,13 @@
 import express from 'express'
+import expressGraphQL from 'express-graphql'
+import schema from '../schema/schema'
 
 const app = express()
+
+app.use('/explore', expressGraphQL({
+  schema,
+  graphiql: true
+}))
 
 app.listen(4000, () => {
   console.info(`App is running on http://localhost:4000`)
